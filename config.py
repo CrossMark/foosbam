6972b4ec76122612ef3ab5ceb7d29ccb15375058
Mark van Kruistum <mkruistum@gmail.com>
2023-12-09 21:27:12 +0100

    use sqllite for local dev

--- config.py
+++ config.py
@@ -1,8 +1,10 @@
 import os
 import urllib
 
-params = urllib.parse.quote_plus(os.environ.get('AZURE_SQL_CONNECTION_STRING'))
+basedir = os.path.abspath(os.path.dirname(__file__))
+#params = urllib.parse.quote_plus(os.environ.get('AZURE_SQL_CONNECTION_STRING'))
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = f"mssql+pyodbc:///?odbc_connect={params}"
+    #SQLALCHEMY_DATABASE_URI = f"mssql+pyodbc:///?odbc_connect={params}"
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
