cd6bc1d0f381de81b75302c8c57dd97b77bfa2ce
Mark van Kruistum <mkruistum@gmail.com>
2023-12-09 22:56:20 +0100

    fix mysql conn

--- config.py
+++ config.py
@@ -1,10 +1,8 @@
 import os
-import urllib
+from urllib.parse import quote_plus
 
 basedir = os.path.abspath(os.path.dirname(__file__))
-#params = urllib.parse.quote_plus(os.environ.get('AZURE_SQL_CONNECTION_STRING'))
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
-    #SQLALCHEMY_DATABASE_URI = f"mssql+pyodbc:///?odbc_connect={params}"
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_DATABASE_URI = os.environ.get(quote_plus('DATABASE_URL')) or 'sqlite:///' + os.path.join(basedir, 'app.db')
