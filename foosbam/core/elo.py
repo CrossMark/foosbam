e7441c248e72790f525fbf375a95c9641c74e042
Mark van Kruistum <mkruistum@gmail.com>
2023-12-18 23:43:09 +0100

    functions done

--- foosbam/core/elo.py
+++ foosbam/core/elo.py
@@ -23,3 +23,60 @@
 # -------------
 # When a new player registers, add a record for this player in the 3 ratings tables, with the default rating.
 # Functionality for recalculating ELO scores over all (accepted) games
+
+import math
+import pandas as pd
+
+players = [
+    {"name": "mark",   "role": "att_black", "team": "black", "rating": 1500, 'num_games': 200},
+    {"name": "wessel", "role": "def_black", "team": "black", "rating": 1500, 'num_games': 130},
+    {"name": "robin",  "role": "att_white", "team": "white", "rating": 1200, 'num_games': 270},
+    {"name": "joeke",  "role": "def_white", "team": "white", "rating": 1300, 'num_games': 245}
+]
+
+df = pd.DataFrame.from_records(players)
+
+def get_opponent_ratings(df, row):
+    own_team = row['team']
+    opp_ratings = df[df['team'] != own_team]['rating'].tolist()
+    return opp_ratings
+
+def calculate_expected_score(rating_player, rating_opponent):
+    prob_player = 1 / (1 + 10 ** ((rating_opponent - rating_player) / 400))
+    return prob_player
+
+def calculate_expected_player_score(row):
+    own_rating = row['rating']
+    exp_score = 0
+    for opp_rating in row['opp_ratings']:
+        exp_score = exp_score + calculate_expected_score(own_rating, opp_rating)
+    exp_score = exp_score / 2
+    return exp_score
+
+def calculate_k_factor(row):
+    return 50 / (1 + row['num_games'] / 300)
+
+def calculate_point_factor(score_black, score_white):
+    score_difference = abs(score_black - score_white)
+    return 2 + (math.log(score_difference + 1) / math.log(10)) ** 3
+
+def get_winner(score_black, score_white):
+    if score_black > score_white:
+        return "black"
+    else:
+        return "white"
+
+def calculate_rating(row, point_factor, winner):
+    if row['team'] == winner:
+        return int(round(row['rating'] + row['k_factor'] * point_factor  * (1 - row['player_expected']), 0))
+    else:
+        return int(round(row['rating'] + row['k_factor'] * point_factor  * (0 - row['player_expected']), 0))
+
+df['opp_ratings'] = df.apply(lambda x : get_opponent_ratings(df, x), axis=1)
+df['player_expected'] = df.apply(lambda x : calculate_expected_player_score(x), axis=1)
+df['k_factor'] = df.apply(lambda x : calculate_k_factor(x), axis=1)
+point_factor = calculate_point_factor(10, 8)
+winner = get_winner(10, 8)
+df['new_rating'] = df.apply(lambda x : calculate_rating(x, point_factor, winner), axis=1)
+
+print(df)
