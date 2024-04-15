2023d2a991b3f8dd44bd07a116f6152514811c37
Mark van Kruistum <mkruistum@gmail.com>
2024-04-15 21:08:39 +0200

    db changes

--- foosbam/auth/routes.py
+++ foosbam/auth/routes.py
@@ -4,7 +4,7 @@ from foosbam import db
 from foosbam.auth import bp
 from foosbam.auth.forms import LoginForm, RegistrationForm, ResetPasswordForm, RequestPasswordResetForm
 from foosbam.email import send_password_reset
-from foosbam.models import User, Rating, Rating_att, Rating_def
+from foosbam.models import User, Rating
 import sqlalchemy as sa
 from urllib.parse import urlsplit
 
@@ -60,11 +60,7 @@ def register():
 
         # add initial rating for new user to ratings table
         rating = Rating(user_id=user.id, rating=1500)
-        rating_att = Rating_att(user_id=user.id, rating=1500)
-        rating_def = Rating_def(user_id=user.id, rating=1500)
         db.session.add(rating)
-        db.session.add(rating_att)
-        db.session.add(rating_def)
         db.session.commit()
 
         flash(f"Have fun with Foosbam, {user.username.title()}!", "is-success")
