dfc7b8353b71520fbf5d2f62b291dd02f855e6c3
Mark van Kruistum <mkruistum@gmail.com>
2024-05-21 21:19:07 +0200

    create season ranking page

--- foosbam/core/ranking.py
+++ foosbam/core/ranking.py
@@ -2,7 +2,7 @@ from foosbam import db
 from foosbam.core import misc
 from foosbam.models import Rating, User
 import pandas as pd
-from sqlalchemy import and_
+from sqlalchemy import and_, func
 from sqlalchemy.orm import aliased
 
 def get_current_ranking():
@@ -62,30 +62,35 @@ def get_current_ranking():
     return df
 
 def get_season_ranking(season):
-
-# ADD SOMETHING LIKE A SUBQUERY HERE TO FIRST FILTER r1 and r2 on the correct season!
-# Maybe also include season 0 always!
-
     r1 = aliased(Rating)
-    r2 = aliased(Rating)
 
+    # In this subquery we first get the latest rating for the given season for each user.
+    # Furthermore we count the number of matches for a user in the given season.
+    latest_season_rating_subquery = db.session.query(
+        r1.user_id,
+        func.count(r1.match_id).label('match_count'),
+        func.max(r1.since).label('max_since')
+    ).filter(
+        r1.season == season
+    ).group_by(
+        r1.user_id
+    ).subquery()
+
+    # Join the actual rating and the User table to the subquery
     ranking = db.session.query(
         r1.since,
         User.id,
         User.username,
         r1.rating_season,
     ).join(
-        r2,
-        and_(r1.user_id == r2.user_id,
-             r1.since < r2.since,
+        latest_season_rating_subquery,
+        and_(r1.user_id == latest_season_rating_subquery.c.user_id,
+             r1.since == latest_season_rating_subquery.c.max_since,
+             latest_season_rating_subquery.c.match_count >= 5           # only include ratings for players with a match count of 5 and higher
             ),
-        isouter = True
     ).join(
         User,
         r1.user_id == User.id,
-        isouter = True
-    ).filter(
-        r2.user_id.is_(None),
     ).order_by(
         r1.rating_season.desc()
     ).order_by(
@@ -109,15 +114,16 @@ def get_season_ranking(season):
 
     df = pd.DataFrame.from_records(ranking_as_dict)
 
-    # Add rank column
-    df['rank'] = df['rating'].rank(method='min', ascending=False).astype(int)
+    if len(df) > 0:
+        # Add rank column
+        df['rank'] = df['rating'].rank(method='min', ascending=False).astype(int)
 
-    # Change since column to Amsterdam time (for frontend) and in desired format
-    df['since'] = df['since'].apply(lambda x : misc.change_timezone(x, 'Etc/UTC', 'Europe/Amsterdam'))
-    df['since'] = df['since'].dt.strftime('%Y-%m-%d %H:%M')
+        # Change since column to Amsterdam time (for frontend) and in desired format
+        df['since'] = df['since'].apply(lambda x : misc.change_timezone(x, 'Etc/UTC', 'Europe/Amsterdam'))
+        df['since'] = df['since'].dt.strftime('%Y-%m-%d %H:%M')
 
-    # Use the title function on the player names, so they get capitals
-    df['player'] = df['player'].str.title()
+        # Use the title function on the player names, so they get capitals
+        df['player'] = df['player'].str.title()
 
     return df
 
