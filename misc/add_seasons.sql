4571fdd780b316d0008f769c7eb1a8c3e1038e80
Mark van Kruistum <mkruistum@gmail.com>
2024-05-23 22:28:34 +0200

    another quick fix

--- misc/add_seasons.sql
+++ misc/add_seasons.sql
@@ -1,17 +1,14 @@
--- database: ../app.sqlite
 -- This is a SQL script to add seasons to the matches and the ratings table.
 -- Normally this happens automatically, but the first time, this column has to be initialized manually.
 
 UPDATE matches
 SET season = CASE
-                WHEN played_at IS NULL          THEN 1
                 WHEN played_at < '2024-04-01'   THEN 1
                 ELSE TIMESTAMPDIFF(QUARTER, '2024-01-01' , played_at) + 1 
             END;
 
 UPDATE ratings
 SET season = CASE
-                WHEN since IS NULL          THEN 1
                 WHEN since < '2024-04-01'   THEN 1
                 ELSE TIMESTAMPDIFF(QUARTER, '2024-01-01' , since) + 1
             END;
